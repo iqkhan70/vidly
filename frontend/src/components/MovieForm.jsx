5433fd45edd4f37d2425e7a02bc23da52847a18a
Iqbal <iqbal.khan@netimpactstrategies.com>
2022-09-29 22:31:06 -0500

    4

--- frontend/src/components/MovieForm.jsx
+++ frontend/src/components/MovieForm.jsx
@@ -20,7 +20,7 @@ function MovieForm({ onAddMovie }) {
       <Input
         aria-label="New Movie"
         onChange={handleChange}
-        placeholder="Add a new movie...4"
+        placeholder="Add a new movie...44"
         type="text"
         value={title}
       />
