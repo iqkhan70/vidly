c970633e4dced71f7db154cd2f4c065f31daaad1
Iqbal <iqbal.khan@netimpactstrategies.com>
2022-10-03 18:11:32 -0500

    backend lib addition

--- frontend/src/components/MovieForm.jsx
+++ frontend/src/components/MovieForm.jsx
@@ -20,7 +20,7 @@ function MovieForm({ onAddMovie }) {
       <Input
         aria-label="New Movie"
         onChange={handleChange}
-        placeholder="Add a new movie...5"
+        placeholder="Add a new movie...55"
         type="text"
         value={title}
       />
